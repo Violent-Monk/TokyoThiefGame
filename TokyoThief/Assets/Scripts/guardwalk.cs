3b9b94bc825028ba7d6b99f506a3a053c5d7a858
Kae-EmTee <Kae-EmTee@users.noreply.github.com>
2020-03-09 03:05:09 -0700

    Audio

---
+++ TokyoThief/Assets/Scripts/guardwalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardsteps : MonoBehaviour
+{
+
+	private AudioSource guardswalk;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        guardswalk = GetComponent<AudioSource>(); 
+		guardswalk.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ TokyoThief/Assets/Scripts/hirowalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walking : MonoBehaviour
+{
+
+private AudioSource footsteps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		footsteps = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     	if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)){
+			footsteps.Play(); //footsteps on press
+		}
+		
+		if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)){
+			footsteps.Stop(); //NEED
+		}
+    }
+}
