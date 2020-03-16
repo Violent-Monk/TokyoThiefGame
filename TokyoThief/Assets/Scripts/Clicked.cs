89295de944af675cb215af41a101ba2887ceaa29
Kae-EmTee <Kae-EmTee@users.noreply.github.com>
2020-03-15 19:12:32 -0700

    Audio

---
+++ TokyoThief/Assets/Scripts/Clicked.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicked : MonoBehaviour
+{
+
+private AudioSource clickoption;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickoption = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+			clickoption.Play();
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			clickoption.Stop();
+		}
+    }
+}
---
+++ TokyoThief/Assets/Scripts/GuardFootsteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardFootsteps : MonoBehaviour
+{
+
+private AudioSource guards;
+public AudioRolloffMode Linear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        guards = GetComponent<AudioSource>();
+		guards.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
