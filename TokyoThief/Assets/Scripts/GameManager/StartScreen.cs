3108e942f939bd0c1c80bc62c9b5cf0f9040750a
Jimmy <jimmytoors@gmail.com>
2020-04-05 21:04:00 -0700

    Release candidate

---
+++ TokyoThief/Assets/Scripts/GameManager/StartScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    GameObject startScreen;
+    GameObject mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScreen = GameObject.Find("StartScreen");
+        mainMenu = GameObject.Find("MainMenuCanvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            mainMenu.GetComponent<Canvas>().sortingOrder = 1;
+            startScreen.SetActive(false);           
+        }
+    }
+}
